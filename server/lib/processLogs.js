14b816d3457dc69eaba6867524cebea9613f4841
Oleksandr Zarubin <oleksandr.zarubin@ua.akvelon.com>
2017-04-11 11:01:01 +0300

    tools added

---
+++ server/lib/processLogs.js
@@ -0,0 +1,75 @@
+const async = require('async');
+const moment = require('moment');
+const Logentries = require('logs-to-logentries');
+
+const loggingTools = require('auth0-log-extension-tools');
+const config = require('../lib/config');
+const logger = require('../lib/logger');
+
+module.exports = (storage) =>
+  (req, res, next) => {
+    if (!req.body || !req.body.schedule || req.body.state !== 'active') {
+      return next();
+    }
+
+    // SETUP LOGENTRIES CLIENT
+    const endpoint = 'https://webhook.logentries.com/noformat/logs/',
+      logentriesToken = config('LOGENTRIES_TOKEN'),
+      logentries = Logentries.createClient({
+        url: endpoint + logentriesToken
+      });
+
+    const onLogsReceived = (logs, callback) => {
+      if (!logs || !logs.length) {
+        return callback();
+      }
+
+      logger.info('Uploading blobs...');
+
+      async.eachLimit(logs, 5, (log, cb) => {
+        const date = moment(log.date);
+        const url = `${date.format('YYYY/MM/DD')}/${date.format('HH')}/${log._id}.json`;
+        logger.info(`Uploading ${url}.`);
+
+        // logentries here...
+        logentries.log(JSON.stringify(log), cb);
+
+      }, (err) => {
+        if (err) {
+          return callback(err);
+        }
+
+        logger.info('Upload complete.');
+        return callback();
+      });
+    };
+
+    const slack = new loggingTools.reporters.SlackReporter({
+      hook: config('SLACK_INCOMING_WEBHOOK_URL'),
+      username: 'auth0-logs-to-logentries',
+      title: 'Logs To Logentries'
+    });
+
+    const options = {
+      domain: config('AUTH0_DOMAIN'),
+      clientId: config('AUTH0_CLIENT_ID'),
+      clientSecret: config('AUTH0_CLIENT_SECRET'),
+      batchSize: config('BATCH_SIZE'),
+      startFrom: config('START_FROM'),
+      logTypes: config('LOG_TYPES'),
+      logLevel: config('LOG_LEVEL')
+    };
+
+    const auth0logger = new loggingTools.LogsProcessor(storage, options);
+
+    return auth0logger
+      .run(onLogsReceived)
+      .then(result => {
+        slack.send(result.status, result.checkpoint);
+        res.json(result);
+      })
+      .catch(err => {
+        slack.send({ error: err, logsProcessed: 0 }, null);
+        next(err);
+      });
+  };
